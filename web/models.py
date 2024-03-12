bf9d7ed75c2686e9d2de71edca0e661e34b891c7
angel <alm39@alumnes.udl.cat>
2024-03-12 17:30:41 +0100

    commit de prova

--- web/models.py
+++ web/models.py
@@ -3,36 +3,22 @@ from django.db import models
 # This is an example of a model in Django so that you can see how you can define your own models :D
 # DON'T FORGET to run `python manage.py makemigrations` and `python manage.py migrate` after changing this file!!!
 class Song(models.Model):
-    title = models.CharField(max_length=10000)
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-    birth_date = models.DateField()
-
-    def __str__(self):
-        return self.name
-
-class Publisher(models.Model):
-    name = models.CharField(max_length=200)
-    founded = models.DateField()
-    city = models.CharField(max_length=60)
-
-    def __str__(self):
-        return self.name
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    publication_date = models.DateField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    genre = models.CharField(max_length=100)
-    publishers = models.ManyToManyField(Publisher, related_name='books')
-
+    title = models.CharField(max_length=100)
+    album = models.CharField(max_length=100)
+    duration = models.DurationField()
+    lyrics = models.TextField()
+    artist = models.ManyToManyField('Artist', on_delete=models.CASCADE)
+    genre = models.ManyToManyField('Genre', on_delete=models.)
     def __str__(self):
         return self.title
-
-class Review(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    review_text = models.TextField()
-    rating = models.IntegerField()
-
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    monthly_listeners = models.IntegerField()
     def __str__(self):
-        return f"{self.book.title} - {self.rating}/10"
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
