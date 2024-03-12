b60ec49a3dc4eb46b95091aa80d2552c726cfebf
joelblanc <joelblanciniesta@gmail.com>
2024-03-12 17:55:32 +0100

    commit

--- web/admin.py
+++ web/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 
 # This is how you can register your models with the Django admin so that you can manage them from the admin interface
 
-from .models import Author, Publisher, Book, Review
+from .models import Song, Artist, Genre
 
-admin.site.register(Author)
-admin.site.register(Publisher)
-admin.site.register(Book)
-admin.site.register(Review)
+admin.site.register(Song)
+admin.site.register(Artist)
+admin.site.register(Genre)
