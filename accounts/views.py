5286a6c0497bc2e887ae7e313fd2e3c2473b4d3d
lau <lauraamarti03@gmail.com>
2024-03-19 19:22:56 +0100

    login Page

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 
+
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login") # Redirect to the login page after a successful registration
