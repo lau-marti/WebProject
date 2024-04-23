4deed70de37fdee08f0001ab4130ebd873d467f9
miqueljordan <miqueljordan11@gmail.com>
2024-04-23 18:08:21 +0200

    JavaScript searchFiveSongs v1.1 Detalls: Fer que la song a buscar s'introdueixi per parametres

--- static/javascript/searchFiveSongs.js
+++ static/javascript/searchFiveSongs.js
@@ -1,5 +1,4 @@
 const SpotifyWebApi = require('spotify-web-api-node');
-const readline = require('readline');
 
 // Configurar credenciales de cliente de Spotify
 const client_id = '7f44fa05891c4b0388022a3317aea4cb';
@@ -22,47 +21,47 @@ async function obtenerToken() {
 }
 
 // Función para buscar información de una canción
-async function buscarCancion() {
+async function buscarCancion(nombreCancion) {
   await obtenerToken();
 
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+  try {
+    const response = await spotifyApi.searchTracks('track:' + nombreCancion, { limit: 5 });
 
-  rl.question('Introduce el nombre de la canción: ', async (nombreCancion) => {
-    rl.close();
+    if (response.body.tracks.items.length > 0) {
+      console.log("Se encontraron las siguientes canciones:");
 
-    try {
-      const response = await spotifyApi.searchTracks('track:' + nombreCancion, { limit: 5 });
+      for (let i = 0; i < response.body.tracks.items.length; i++) {
+        const cancion = response.body.tracks.items[i];
+        console.log(`Canción ${i + 1}:`);
+        console.log("Nombre:", cancion.name);
+        console.log("Artista(s):", cancion.artists.map(artist => artist.name).join(", "));
+        console.log("Álbum:", cancion.album.name);
+        console.log("URL del álbum:", cancion.album.external_urls.spotify);
+        console.log("URL de la imagen del álbum:", cancion.album.images[0].url);
 
-      if (response.body.tracks.items.length > 0) {
-        console.log("Se encontraron las siguientes canciones:");
+        // Obtener los géneros de los artistas asociados
+        const artistasIds = cancion.artists.map(artist => artist.id);
+        const artistasInfo = await Promise.all(artistasIds.map(artistId => spotifyApi.getArtist(artistId)));
 
-        for (let i = 0; i < response.body.tracks.items.length; i++) {
-          const cancion = response.body.tracks.items[i];
-          console.log(`Canción ${i + 1}:`);
-          console.log("Nombre:", cancion.name);
-          console.log("Artista(s):", cancion.artists.map(artist => artist.name).join(", "));
-          console.log("Álbum:", cancion.album.name);
-          console.log("URL del álbum:", cancion.album.external_urls.spotify);
-          console.log("URL de la imagen del álbum:", cancion.album.images[0].url);
-
-          // Obtener los géneros de los artistas asociados
-          const artistasIds = cancion.artists.map(artist => artist.id);
-          const artistasInfo = await Promise.all(artistasIds.map(artistId => spotifyApi.getArtist(artistId)));
-
-          console.log("Género(s):", artistasInfo.map(artistInfo => artistInfo.body.genres.join(", ")).join(", "));
-          console.log("--------------------");
-        }
-      } else {
-        console.log("No se encontraron canciones con ese nombre.");
+        console.log("Género(s):", artistasInfo.map(artistInfo => artistInfo.body.genres.join(", ")).join(", "));
+        console.log("--------------------");
       }
-    } catch (error) {
-      console.error("Error al buscar la canción:", error);
+    } else {
+      console.log("No se encontraron canciones con ese nombre.");
     }
-  });
+  } catch (error) {
+    console.error("Error al buscar la canción:", error);
+  }
 }
 
-// Ejemplo de uso
-buscarCancion();
+// Obtener el nombre de la canción de los argumentos de la línea de comandos
+const nombreCancion = process.argv[2];
+
+// Verificar si se proporcionó el nombre de la canción como argumento
+if (nombreCancion) {
+  // Si se proporcionó el nombre de la canción, llamar a la función buscarCancion con ese nombre
+  buscarCancion(nombreCancion);
+} else {
+  // Si no se proporcionó el nombre de la canción, mostrar un mensaje de error
+  console.error("Por favor, proporciona el nombre de la canción como argumento.");
+}
