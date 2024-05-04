feed2018bde2ab0d66019d8ec97c09731a1be560
miqueljordan <miqueljordan11@gmail.com>
2024-05-04 10:48:35 +0200

    Ara el programa espera a que la BD estigui creada abans de guardar a la BD els generes
    Modificat apps.py

--- web/apps.py
+++ web/apps.py
@@ -1,6 +1,23 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from django.core.management import call_command
 
 
 class WebConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'web'
+
+    def ready(self):
+        pass
+
+
+@receiver(post_migrate)
+def load_genres(sender, **kwargs):
+    # Importamos el modelo Genre aquí para evitar la referencia cruzada
+    from .models import Genre
+
+    # Verificamos si el modelo Genre ya existe en la base de datos
+    if not Genre.objects.exists():
+        call_command('load_genres')
+
