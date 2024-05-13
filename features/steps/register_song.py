9d633500cc83e336a5dc75abf5af0411015cd13d
lau <lauraamarti03@gmail.com>
2024-05-13 12:10:02 +0200

    vL2.6: Error 403 + testos edit_playlist.feature + regist_song.feature

--- features/steps/register_song.py
+++ features/steps/register_song.py
@@ -1,3 +1,4 @@
+import time
 from functools import reduce
 
 from behave import *
@@ -9,35 +10,48 @@ from project.settings import BASE_DIR
 
 use_step_matcher("parse")
 
+
 @given('Exists song at playlist "{playlist_name}" by "{username}"')
 def step_impl(context, playlist_name, username):
     from django.contrib.auth.models import User
+    from web.models import Playlist, Song, Artist
+
     user = User.objects.get(username=username)
-    from web.models import Playlist
     playlist = Playlist.objects.get(name=playlist_name)
-    from web.models import Song
+
     for row in context.table:
         song = Song.objects.create(title=row['name'])
-        playlist.songs.add(song)
-        for heading in row.headings:
-            setattr(song, heading, row[heading])
-        song.save()
+        # Asignación de valores a las columnas presentes en la tabla
+        if 'album' in row.headings:
+            song.album = row['album']
+        if 'duration' in row.headings:
+            song.duration = row['duration']
+        if 'URL' in row.headings:
+            song.URL = row['URL']
+        if 'artists' in row.headings:
+            for artist_name in row['artists'].split(','):
+                artist, _ = Artist.objects.get_or_create(name=artist_name.strip())
+                song.artists.add(artist)
 
-@when('I register song at playlist "{playlist_name}"')
-def step_impl(context, playlist_name):
+        song.save()
+        playlist.songs.add(song)
+
+
+@when('I search and add song "{song_title}" to playlist "{playlist_name}"')
+def step_impl(context, song_title, playlist_name):
     from web.models import Playlist
     playlist = Playlist.objects.get(name=playlist_name)
-    for row in context.table:
-        context.browser.visit(context.get_url('web:song_create', playlist.pk))
-        if context.browser.url == context.get_url('web:song_create', playlist.pk):
-            form = context.browser.find_by_id('input-form').first
-            for heading in row.headings:
-                if heading == 'image':
-                    filePath = os.path.join(BASE_DIR, row[heading])
-                    context.browser.fill(heading, filePath)
-                else:
-                    context.browser.fill(heading, row[heading])
-            form.find_by_value('Submit').first.click()
+    context.browser.visit(context.get_url('web:song_create', playlist.pk))
+    if context.browser.url == context.get_url('web:song_create', playlist.pk):
+        context.browser.fill('search', song_title)
+        context.browser.find_by_css('div#content button').click()
+        # Select the song from the search results
+        search_results = context.browser.find_by_id('searchResults')
+        time.sleep(2)
+        context.browser.find_by_id('add_0').click()
+        time.sleep(2)
+
+
 
 @then('I\'m viewing the details page for song at playlist "{playlist_name}" by "{username}"')
 def step_impl(context, playlist_name, username):
--- web/management/commands/load_genres.py
+++ web/management/commands/load_genres.py
@@ -6,13 +6,16 @@ from spotipy.oauth2 import SpotifyClientCredentials
 class Command(BaseCommand):
     help = 'Carga los géneros de música desde Spotify y los guarda en un archivo de texto'
 
-    def handle(self, *args, **kwargs):
-        # Tu código para cargar los géneros de música y guardarlos en el archivo de texto
-        client_id = '7f44fa05891c4b0388022a3317aea4cb'
-        client_secret = '3f2c3b851b4a4973b44ae04c7bbaf4b7'
-        client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-        sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-        generos_spotify = sp.recommendation_genre_seeds()
+    # def handle(self, *args, **kwargs):
+    #     # Tu código para cargar los géneros de música y guardarlos en el archivo de texto
+    #     client_id = '7f44fa05891c4b0388022a3317aea4cb'
+    #     client_secret = '3f2c3b851b4a4973b44ae04c7bbaf4b7'
+    #     client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+    #     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    #     generos_spotify = sp.recommendation_genre_seeds()
+    #
+    #     for genero in generos_spotify['genres']:
+    #         Genre.objects.get_or_create(name=genero)
 
-        for genero in generos_spotify['genres']:
-            Genre.objects.get_or_create(name=genero)
+    def handle(self, *args, **kwargs):
+        Genre.objects.get_or_create(name="pop")
