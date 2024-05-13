ad77c7691536736b6548a8f7bbbe0fe278ca3ed5
lau <lauraamarti03@gmail.com>
2024-05-13 20:48:24 +0200

    vL2.10: Funciona tot

--- features/steps/view_song.py
+++ features/steps/view_song.py
@@ -1,9 +1,13 @@
 import time
+from datetime import timedelta
 
 from behave import *
+from django.contrib.auth.models import User
+
 from web.models import Playlist, Song
 
 use_step_matcher("parse")
+
 
 @step('I click on the button with name "{playlist_name}"')
 def step_impl(context, playlist_name):
@@ -30,3 +34,30 @@ def step_impl(context, song_name):
     from web.models import Song
     song = Song.objects.get(title=song_name)
     context.browser.url.endswith("/songs/"+str(song.id))
+
+@given('Playlist "{playlist_song}" registered by "{user}" contains the songs')
+def add_songs_to_playlist(context, playlist_song, user):
+    for row in context.table:
+        title = row["song_title"]
+        album = row["album"]
+        duration_str = row["duration"]
+        duration_parts = duration_str.split(":")
+        duration = timedelta(
+            hours=int(duration_parts[0]),
+            minutes=int(duration_parts[1]),
+            seconds=int(duration_parts[2])
+        )
+        song = Song.objects.create(
+            title=title,
+            album=album,
+            duration=duration
+        )
+        context.playlist.songs.add(song)
+
+@given('Create playlist "{playlist_name}" registered by "{username}"')
+def create_playlist(context, playlist_name, username):
+    user = User.objects.get(username=username)
+
+    playlist = Playlist.objects.create(user=user, name=playlist_name)
+
+    context.playlist=playlist
